da2d5fa939463fe06b4420670f897742e8dbbd33
Johnny6191 <uzzylak123@gmail.com>
2024-05-24 00:56:07 +0100

    Update DeepL Translator (click here).bat

--- DeepL Translator (click here).bat
+++ DeepL Translator (click here).bat
@@ -1,16 +1,3 @@
-pushd %~dp0
-
-cd backendServer
-cd Modules
-cd DeepL-Server
-
-start /min cmd /c "activate.bat" 
-
-cd ..
-cd ..
-cd ..
-
-cd userInterface
-start userInterface.exe
+<a class="prev_nav page_prev"></a>
 
 
