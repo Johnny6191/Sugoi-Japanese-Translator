bdd1a4d77bdd74e49eb458f74730a7dec975174f
Johnny6191 <uzzylak123@gmail.com>
2024-05-24 00:52:17 +0100

    Update DeepL Translator (click here).bat

--- DeepL Translator (click here).bat
+++ DeepL Translator (click here).bat
@@ -1,16 +1,3 @@
-pushd %~dp0
-
-cd backendServer
-cd Modules
-cd DeepL-Server
-
-start /min cmd /c "activate.bat" 
-
-cd ..
-cd ..
-cd ..
-
-cd userInterface
-start userInterface.exe
+<img id="gimg" class="image_14" src="//m3.hentaiera.com/012/xkrl2azeo6/14.jpg" data-src="https://m3.hentaiera.com/012/xkrl2azeo6/1.jpg" alt="Tifa to Boxing, Shiyo side:S page 1 full" style="max-width: 1280px; max-height: 673px; margin: 0px auto; display: block;" data-ll-status="loaded">
 
 
